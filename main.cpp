37fd1a5f13b0192a83ccac1296f6be4da90bc304
krabradosty <krabradosty@gmail.com>
2016-09-15 01:17:44 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1F.h>
+#include "TApplication.h"
+
+using namespace std;
+
+int main() {
+  TApplication *myapp = new TApplication("myapp",0,0);
+  Double_t mupeTime[200000];
+  Int_t muNpe;
+
+
+  TFile* inputFile = new TFile("/home/krab/out_ds20kwt_result.root");
+  TTree* tree =(TTree*) inputFile->Get("dstree");
+  TFile* outputFile = new TFile("/home/krab/out.root", "RECREATE");
+  TH1F* hMuTime = new TH1F("hMuTime", "Time distribution of photoelectron registration;t_{pe} [ns];number", 500, 0, 1500);
+
+  tree->SetBranchAddress("munpe", &muNpe);
+  tree->SetBranchAddress("mupe_time", mupeTime);
+  auto nentries = tree->GetEntries();
+  for (long i = 0; i < nentries; i++) {
+    tree->GetEntry(i);
+    for (int j=0; j < muNpe; j++) {
+      hMuTime->Fill(mupeTime[j]);
+    }
+  }
+
+  outputFile->Write();
+  outputFile->Close();
+  inputFile->Close();
+
+  return 0;
+}
